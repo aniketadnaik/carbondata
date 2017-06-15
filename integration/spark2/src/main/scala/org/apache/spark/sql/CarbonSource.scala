73053b9fef5d24430fe671fdd9abe098658a31ed
Aniket Adnaik <aniket.adnaik@gmail.com>
2017-06-15 11:57:43 -0700

    [CARBONDATA-1173] Stream ingestion - write path framework
    This closes #1064

--- integration/spark2/src/main/scala/org/apache/spark/sql/CarbonSource.scala
+++ integration/spark2/src/main/scala/org/apache/spark/sql/CarbonSource.scala
@@ -17,30 +17,38 @@
 
 package org.apache.spark.sql
 
+import java.io.{BufferedWriter, FileWriter, IOException}
+import java.util.UUID
+
 import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
 import scala.language.implicitConversions
 
 import org.apache.commons.lang.StringUtils
 import org.apache.hadoop.fs.{FileStatus, Path}
 import org.apache.hadoop.mapreduce.Job
+import org.apache.parquet.schema.InvalidSchemaException
 import org.apache.spark.sql.catalyst.analysis.NoSuchTableException
 import org.apache.spark.sql.catalyst.catalog.CatalogTable
 import org.apache.spark.sql.execution.CarbonLateDecodeStrategy
-import org.apache.spark.sql.execution.command.{TableModel, TableNewProcessor}
+import org.apache.spark.sql.execution.command.{CreateTable, TableModel, TableNewProcessor}
 import org.apache.spark.sql.execution.datasources.{FileFormat, OutputWriterFactory}
-import org.apache.spark.sql.hive.{CarbonMetaStore, CarbonRelation}
+import org.apache.spark.sql.hive.{CarbonMetastore, CarbonRelation}
 import org.apache.spark.sql.optimizer.CarbonLateDecodeRule
 import org.apache.spark.sql.parser.CarbonSpark2SqlParser
 import org.apache.spark.sql.sources._
 import org.apache.spark.sql.streaming.CarbonStreamingOutputWriterFactory
 import org.apache.spark.sql.types.{StringType, StructType}
 
+import org.apache.carbondata.common.logging.LogServiceFactory
 import org.apache.carbondata.core.constants.CarbonCommonConstants
+import org.apache.carbondata.core.datastore.impl.FileFactory
 import org.apache.carbondata.core.metadata.AbsoluteTableIdentifier
 import org.apache.carbondata.core.metadata.schema.SchemaEvolutionEntry
 import org.apache.carbondata.core.metadata.schema.table.TableInfo
-import org.apache.carbondata.core.util.path.{CarbonStorePath, CarbonTablePath}
 import org.apache.carbondata.core.util.{CarbonProperties, CarbonUtil}
+import org.apache.carbondata.core.util.path.{CarbonStorePath, CarbonTablePath}
+import org.apache.carbondata.format.{DataType, TableInfo, TableSchema}
 import org.apache.carbondata.spark.CarbonOption
 import org.apache.carbondata.spark.exception.MalformedCarbonCommandException
 
@@ -52,7 +60,11 @@ import org.apache.carbondata.spark.exception.MalformedCarbonCommandException
 class CarbonSource extends CreatableRelationProvider with RelationProvider
   with SchemaRelationProvider with DataSourceRegister with FileFormat  {
 
-  override def shortName(): String = "carbondata"
+  private val LOGGER = LogServiceFactory.getLogService(this.getClass.getCanonicalName)
+
+  override def shortName(): String = {
+    "carbondata"
+  }
 
   // will be called if hive supported create table command is provided
   override def createRelation(sqlContext: SQLContext,
@@ -179,14 +191,13 @@ class CarbonSource extends CreatableRelationProvider with RelationProvider
     }
   }
 
-  /**
-   * Returns the path of the table
-   *
-     * @param sparkSession
-   * @param dbName
-   * @param tableName
-   * @return
-   */
+/**
+ * Returns the path of the table
+ * @param sparkSession
+ * @param dbName
+ * @param tableName
+ * @return
+ */
   private def getPathForTable(sparkSession: SparkSession, dbName: String,
       tableName : String, parameters: Map[String, String]): (String, Map[String, String]) = {
 
@@ -217,22 +228,189 @@ class CarbonSource extends CreatableRelationProvider with RelationProvider
    * be put here.  For example, user defined output committer can be configured here
    * by setting the output committer class in the conf of spark.sql.sources.outputCommitterClass.
    */
-  def prepareWrite(
-    sparkSession: SparkSession,
-    job: Job,
-    options: Map[String, String],
-    dataSchema: StructType): OutputWriterFactory = new CarbonStreamingOutputWriterFactory()
+  override def prepareWrite(
+      sparkSession: SparkSession,
+      job: Job,
+      options: Map[String, String],
+      dataSchema: StructType): OutputWriterFactory = {
+
+    // Check if table with given path exists
+    validateTable(options.get("path").get)
+
+    // Check id streaming data schema matches with carbon table schema
+    // Data from socket source does not have schema attached to it,
+    // Following check is to ignore schema validation for socket source.
+    if (!(dataSchema.size.equals(1) &&
+      dataSchema.fields(0).dataType.equals(StringType))) {
+      val tablePath = options.get("path")
+      val path: String = tablePath match {
+        case Some(value) => value
+        case None => ""
+      }
+      val meta: CarbonMetastore = new CarbonMetastore(sparkSession.conf, path)
+      val schemaPath = path + "/Metadata/schema"
+      val schema: TableInfo = meta.readSchemaFile(schemaPath)
+      val isSchemaValid = validateSchema(schema, dataSchema)
+
+      if(!isSchemaValid) {
+        LOGGER.error("Schema Validation Failed: streaming data schema"
+          + "does not match with carbon table schema")
+        throw new InvalidSchemaException("Schema Validation Failed : " +
+          "streaming data schema does not match with carbon table schema")
+      }
+    }
+    new CarbonStreamingOutputWriterFactory()
+  }
 
   /**
+   * Read schema from existing carbon table
+   * @param sparkSession
+   * @param tablePath carbon table path
+   * @return true if schema validation is successful else false
+   */
+  private def getTableSchema(sparkSession: SparkSession, tablePath: String): TableInfo = {
+    val meta: CarbonMetastore = new CarbonMetastore(sparkSession.conf, tablePath)
+    val schemaPath = tablePath + "/Metadata/schema"
+    val schema: TableInfo = meta.readSchemaFile(schemaPath)
+    schema
+  }
+
+  /**
+   * Validates streamed schema against existing table schema
+   * @param schema existing carbon table schema
+   * @param dataSchema streamed data schema
+   * @return true if schema validation is successful else false
+   */
+  private def validateSchema(schema: TableInfo, dataSchema: StructType): Boolean = {
+    val factTable: TableSchema = schema.getFact_table
+
+    import scala.collection.mutable.ListBuffer
+    var columnnSchemaValues = factTable.getTable_columns.asScala.sortBy(_.schemaOrdinal)
+
+    var columnDataTypes = new ListBuffer[String]()
+    for(columnDataType <- columnnSchemaValues) {
+      columnDataTypes.append(columnDataType.data_type.toString)
+    }
+    val tableColumnDataTypeList = columnDataTypes.toList
+
+    var streamSchemaDataTypes = new ListBuffer[String]()
+    for(i <- 0 until dataSchema.size) {
+      streamSchemaDataTypes
+        .append(
+          mapStreamingDataTypeToString(dataSchema.fields(i).dataType.toString))
+    }
+    val streamedDataTypeList = streamSchemaDataTypes.toList
+
+    val isValid = tableColumnDataTypeList == streamedDataTypeList
+    isValid
+  }
+
+  /**
+   * Parses streamed datatype according to carbon datatype
+   * @param dataType
+   * @return String
+   */
+  def mapStreamingDataTypeToString(dataType: String): String = {
+    dataType match {
+      case "IntegerType" => DataType.INT.toString
+      case "StringType" => DataType.STRING.toString
+      case "DateType" => DataType.DATE.toString
+      case "DoubleType" => DataType.DOUBLE.toString
+      case "FloatType" => DataType.DOUBLE.toString
+      case "LongType" => DataType.LONG.toString
+      case "ShortType" => DataType.SHORT.toString
+      case "TimestampType" => DataType.TIMESTAMP.toString
+    }
+  }
+
+  /**
+   * Validates if given table exists or throws exception
+   * @param String existing carbon table path
+   * @return None
+   */
+  private def validateTable(tablePath: String): Unit = {
+
+    val formattedTablePath = tablePath.replace('\\', '/')
+    val names = formattedTablePath.split("/")
+    if (names.length < 3) {
+      throw new IllegalArgumentException("invalid table path: " + tablePath)
+    }
+    val tableName : String = names(names.length - 1)
+    val dbName : String = names(names.length - 2)
+    val storePath = formattedTablePath.substring(0,
+      formattedTablePath.lastIndexOf
+      (((dbName.concat(CarbonCommonConstants.FILE_SEPARATOR).toString)
+        .concat(tableName)).toString) - 1)
+    val absoluteTableIdentifier: AbsoluteTableIdentifier =
+      new AbsoluteTableIdentifier(storePath,
+        new CarbonTableIdentifier(dbName, tableName,
+          UUID.randomUUID().toString))
+
+    if (!checkIfTableExists(absoluteTableIdentifier)) {
+      throw new NoSuchTableException(dbName, tableName)
+    }
+  }
+
+  /**
+   * Checks if table exists by checking its schema file
+   * @param absoluteTableIdentifier
+   * @return Boolean
+   */
+  private def checkIfTableExists(absoluteTableIdentifier: AbsoluteTableIdentifier): Boolean = {
+    val carbonTablePath: CarbonTablePath = CarbonStorePath
+      .getCarbonTablePath(absoluteTableIdentifier)
+    val schemaFilePath: String = carbonTablePath.getSchemaFilePath
+    FileFactory.isFileExist(schemaFilePath, FileFactory.FileType.LOCAL) ||
+      FileFactory.isFileExist(schemaFilePath, FileFactory.FileType.HDFS) ||
+      FileFactory.isFileExist(schemaFilePath, FileFactory.FileType.VIEWFS)
+  }
+
+  /**
+   * If user wants to stream data from carbondata table source
+   * and if following conditions are true:
+   *    1. No schema provided by the user in readStream()
+   *    2. spark.sql.streaming.schemaInference is set to true
+   * carbondata can infer a table schema from a valid table path
+   * The schema inference is not mandatory, but good have.
    * When possible, this method should return the schema of the given `files`.  When the format
    * does not support inference, or no valid files are given should return None.  In these cases
    * Spark will require that user specify the schema manually.
    */
-  def inferSchema(
-    sparkSession: SparkSession,
-    options: Map[String, String],
-    files: Seq[FileStatus]): Option[StructType] = Some(new StructType().add("value", StringType))
+  override def inferSchema(
+                   sparkSession: SparkSession,
+                   options: Map[String, String],
+                   files: Seq[FileStatus]): Option[StructType] = {
+    Some(new StructType().add("value", StringType))
+    val path = options.get("path")
+    val tablePath: String = path match {
+      case Some(value) => value
+      case None => ""
+    }
+    // Check if table with given path exists
+    validateTable(tablePath)
+    val schema: TableInfo = getTableSchema(sparkSession: SparkSession, tablePath: String)
+    val factTable: TableSchema = schema.getFact_table
+    var columnnSchemaValues = factTable.getTable_columns.asScala.sortBy(_.schemaOrdinal)
 
+    val tableColumnNames = new ListBuffer[String]()
+    for (columnName <- columnnSchemaValues) {
+      tableColumnNames.append(columnName.column_name)
+    }
+    val tableColumnNamesList = tableColumnNames.toList
+
+    var columnDataTypes = new ListBuffer[String]()
+    for(columnDataType <- columnnSchemaValues) {
+      columnDataTypes.append(columnDataType.data_type.toString)
+    }
+    val tableColumnDataTypeList = columnDataTypes.toList
+
+    val inferredSchema: Option[StructType] = new Some(new StructType())
+    for (i <- tableColumnNamesList.indices) {
+      inferredSchema.get.add(tableColumnNamesList(i), tableColumnDataTypeList(i))
+    }
+
+    inferredSchema
+  }
 }
 
 object CarbonSource {
